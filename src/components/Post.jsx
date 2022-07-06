35ef0d35227b0aecfc7c25b8da1a365f3c7b96c5
joaopedroalc <joaopedro.ufma@gmail.com>
2022-07-05 22:00:43 -0300

    removendo contextos nao usados do Post e console.log

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -2,23 +2,12 @@ import { useContext } from "react";
 import { PostsContext } from "../contexts/PostsContext";
 
 export default function Post({ deletarPost, editarPost }) {
-  const {
-    user,
-    post,
-    setPost,
-    nome,
-    setNome,
-    email,
-    setEmail,
-    content,
-    setContent,
-  } = useContext(PostsContext);
+  const { content } = useContext(PostsContext);
 
   return (
     <div>
       {content.map((item) => (
         <>
-          {console.log(item)}
           <h1>{item.post}</h1>
           <h2>{item.nome}</h2>
           <h3>{item.email}</h3>
