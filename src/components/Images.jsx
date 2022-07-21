98f89dbade11fa8fccb018d80e8bf1c9c2430113
joaopedroalc <joaopedro.ufma@gmail.com>
2022-07-21 17:04:08 -0300

    img size

--- src/components/Images.jsx
+++ src/components/Images.jsx
@@ -4,7 +4,12 @@ import "antd/dist/antd.css"; // se der erro remove esse css
 export function Images({ img }) {
   return (
     <Image.PreviewGroup>
-      <Image src={img} width={300} />
+      <Image
+        src={img}
+        width={300}
+        height={300}
+        style={{ objectFit: "cover" }}
+      />
     </Image.PreviewGroup>
   );
 }
