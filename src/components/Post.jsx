953d01853275140203b22f854b1e7f354efe9fc4
joaopedroalc <joaopedro.ufma@gmail.com>
2022-07-21 18:27:18 -0300

    correct

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -11,60 +11,57 @@ export default function Post({ deletarPost, editarPost, curtirPost }) {
 
   return (
     <div className={styles.posts}>
-      {content.map(
-        (item) =>
-          item.id !== undefined && (
-            <div key={item.id} className={styles.post}>
-              <Images img={item.imagem} />
-              <h2 className={styles.nome}>{item.nome}</h2>
-              <p className={styles.content}>{item.post}</p>
-              <h3 style={{ display: "none", visibility: "hidden" }}>
-                {item.email}
-              </h3>
-              {user.email === item.email ? (
-                <div className={styles.buttonGroup}>
-                  <Button
-                    variant='outlined'
-                    color='error'
-                    onClick={() => deletarPost(item)}
-                  >
-                    Deletar
-                  </Button>
-                  <Button
-                    variant='outlined'
-                    color='info'
-                    onClick={() => editarPost(item)}
-                  >
-                    Editar
-                  </Button>
+      {content.map((item) => (
+        <div key={item.id} className={styles.post}>
+          <Images img={item.imagem} />
+          <h2 className={styles.nome}>{item.nome}</h2>
+          <p className={styles.content}>{item.post}</p>
+          <h3 style={{ display: "none", visibility: "hidden" }}>
+            {item.email}
+          </h3>
+          {user.email === item.email ? (
+            <div className={styles.buttonGroup}>
+              <Button
+                variant='outlined'
+                color='error'
+                onClick={() => deletarPost(item)}
+              >
+                Deletar
+              </Button>
+              <Button
+                variant='outlined'
+                color='info'
+                onClick={() => editarPost(item)}
+              >
+                Editar
+              </Button>
+            </div>
+          ) : (
+            <>
+              {item.like ? (
+                <div className={styles.groupLike}>
+                  <ThumbUpIcon
+                    onClick={() => curtirPost(item.post)}
+                    className={styles.like}
+                    color='success'
+                  />
+                  <span>Gostei do post</span>
                 </div>
               ) : (
-                <>
-                  {item.like ? (
-                    <div className={styles.groupLike}>
-                      <ThumbUpIcon
-                        onClick={() => curtirPost(item.post)}
-                        className={styles.like}
-                        color='success'
-                      />
-                      <span>Gostei do post</span>
-                    </div>
-                  ) : (
-                    <div className={styles.groupLike}>
-                      <ThumbDownIcon
-                        onClick={() => curtirPost(item.post)}
-                        className={styles.like}
-                        color='error'
-                      />
-                      <span>Não gostei do post</span>
-                    </div>
-                  )}
-                  {/* <p>{item.like ? "1" : "0"}</p> */}
-                </>
+                <div className={styles.groupLike}>
+                  <ThumbDownIcon
+                    onClick={() => curtirPost(item.post)}
+                    className={styles.like}
+                    color='error'
+                  />
+                  <span>Não gostei do post</span>
+                </div>
               )}
-            </div>
-          )
-      )}
+              {/* <p>{item.like ? "1" : "0"}</p> */}
+            </>
+          )}
+        </div>
+      ))}
     </div>
   );
 }
