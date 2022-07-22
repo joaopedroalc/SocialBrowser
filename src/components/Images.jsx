21a3bbb95e90546e4083af11fb6ce0ec9e6c820b
joaopedroalc <joaopedro.ufma@gmail.com>
2022-07-21 21:00:06 -0300

    logica correta item.hasOwnProperty("id")

--- src/components/Images.jsx
+++ src/components/Images.jsx
@@ -2,10 +2,15 @@ import { Image } from "antd";
 import "antd/dist/antd.css"; // se der erro remove esse css
 
 export function Images({ img }) {
+  console.log(img);
   return (
     <Image.PreviewGroup>
       <Image
-        src={img}
+        src={
+          img !== ""
+            ? img
+            : "https://forum.bubble.io/uploads/default/original/3X/1/2/12e944afd917d123319c9074a7e72581785a3b38.png"
+        }
         width={300}
         height={300}
         style={{ objectFit: "cover" }}
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -8,60 +8,64 @@ import { Images } from "./Images";
 
 export default function Post({ deletarPost, editarPost, curtirPost }) {
   const { user, content } = useContext(PostsContext);
+  console.log(content);
 
   return (
     <div className={styles.posts}>
-      {content.map((item) => (
-        <div key={item.id} className={styles.post}>
-          <Images img={item.imagem} />
-          <h2 className={styles.nome}>{item.nome}</h2>
-          <p className={styles.content}>{item.post}</p>
-          <h3 style={{ display: "none", visibility: "hidden" }}>
-            {item.email}
-          </h3>
-          {user.email === item.email ? (
-            <div className={styles.buttonGroup}>
-              <Button
-                variant='outlined'
-                color='error'
-                onClick={() => deletarPost(item)}
-              >
-                Deletar
-              </Button>
-              <Button
-                variant='outlined'
-                color='info'
-                onClick={() => editarPost(item)}
-              >
-                Editar
-              </Button>
-            </div>
-          ) : (
-            <>
-              {item.like ? (
-                <div className={styles.groupLike}>
-                  <ThumbUpIcon
-                    onClick={() => curtirPost(item.post)}
-                    className={styles.like}
-                    color='success'
-                  />
-                  <span>Gostei do post</span>
+      {content.map(
+        (item) =>
+          item.hasOwnProperty("id") && (
+            <div key={item.id} className={styles.post}>
+              <Images img={item.imagem} />
+              <h2 className={styles.nome}>{item.nome}</h2>
+              <p className={styles.content}>{item.post}</p>
+              <h3 style={{ display: "none", visibility: "hidden" }}>
+                {item.email}
+              </h3>
+              {user.email === item.email ? (
+                <div className={styles.buttonGroup}>
+                  <Button
+                    variant='outlined'
+                    color='error'
+                    onClick={() => deletarPost(item)}
+                  >
+                    Deletar
+                  </Button>
+                  <Button
+                    variant='outlined'
+                    color='info'
+                    onClick={() => editarPost(item)}
+                  >
+                    Editar
+                  </Button>
                 </div>
               ) : (
-                <div className={styles.groupLike}>
-                  <ThumbDownIcon
-                    onClick={() => curtirPost(item.post)}
-                    className={styles.like}
-                    color='error'
-                  />
-                  <span>Não gostei do post</span>
-                </div>
+                <>
+                  {item.like ? (
+                    <div className={styles.groupLike}>
+                      <ThumbUpIcon
+                        onClick={() => curtirPost(item.post)}
+                        className={styles.like}
+                        color='success'
+                      />
+                      <span>Gostei do post</span>
+                    </div>
+                  ) : (
+                    <div className={styles.groupLike}>
+                      <ThumbDownIcon
+                        onClick={() => curtirPost(item.post)}
+                        className={styles.like}
+                        color='error'
+                      />
+                      <span>Não gostei do post</span>
+                    </div>
+                  )}
+                  {/* <p>{item.like ? "1" : "0"}</p> */}
+                </>
               )}
-              {/* <p>{item.like ? "1" : "0"}</p> */}
-            </>
-          )}
-        </div>
-      ))}
+            </div>
+          )
+      )}
     </div>
   );
 }
